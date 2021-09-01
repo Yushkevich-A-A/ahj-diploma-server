2c97d37638da47dcf3eebad0f68501a816cde1ab
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-09-01 23:45:48 +0300

    server create

--- DB/db.js
+++ DB/db.js
@@ -1,125 +1,72 @@
 const db = {
-    postsList: [
-        {
-            type: 'text',
-            data: {
-                id: '1982-cskndhjnc-sdcsdhjd-sdokvsdj',
-                content: 'Привет, мир!',
-                date: +Date.now(),
-            }
-        },
-        {
-            type: 'text',
-            data: {
-                id: '1982-cskndhjnc-sdcsdhjd-sdokvsdj',
-                content: 'Привет, мир!',
-                date: +Date.now(),
-            }
-        },
-        {
-            type: 'text',
-            data: {
-                id: '1982-cskndhjnc-sdcsdhjd-sdokvsdj',
-                content: 'Привет, мир!',
-                date: +Date.now(),
-            }
-        },
-        {
-            type: 'text',
-            data: {
-                id: '1982-cskndhjnc-sdcsdhjd-sdokvsdj',
-                content: 'Привет, мир!',
-                date: +Date.now(),
-            }
-        },
-        {
-            type: 'text',
-            data: {
-                id: '1982-cskndhjnc-sdcsdhjd-sdokvsdj',
-                content: 'Привет, мир!',
-                date: +Date.now(),
-            }
-        },
-        {
-            type: 'text',
-            data: {
-                id: '1982-cskndhjnc-sdcsdhjd-sdokvsdj',
-                content: 'Привет, мир!',
-                date: +Date.now(),
-            }
-        },
-        {
-            type: 'text',
-            data: {
-                id: '1982-cskndhjnc-sdcsdhjd-sdokvsdj',
-                content: 'Привет, мир!',
-                date: +Date.now(),
-            }
-        },
-        {
-            type: 'text',
-            data: {
-                id: '1982-cskndhjnc-sdcsdhjd-sdokvsdj',
-                content: 'Привет, мир!',
-                date: +Date.now(),
-            }
-        },
-        {
-            type: 'text',
-            data: {
-                id: '1982-cskndhjnc-sdcsdhjd-sdokvsdj',
-                content: 'Привет, мир!',
-                date: +Date.now(),
-            }
-        },
-        {
-            type: 'text',
-            data: {
-                id: '1982-cskndhjnc-sdcsdhjd-sdokvsdj',
-                content: 'Привет, мир!',
-                date: +Date.now(),
-            }
-        },
-        {
-            type: 'text',
-            data: {
-                id: '1982-cskndhjnc-sdcsdhjd-sdokvsdj',
-                content: 'Привет, мир!',
-                date: +Date.now(),
-            }
-        },
-        {
-            type: 'text',
-            data: {
-                id: '1982-cskndhjnc-sdcsdhjd-sdokvsdj',
-                content: 'Привет, мир!',
-                date: +Date.now() + 5000000,
-            }
-        },
-    ],
+    postId: 0,
 
-    postImage:[],
-    postVideo: [],
+    postsList: [],
+    postsImage:[],
+    postsVideo: [],
+    postsAudio: [],
 
-    addNewPosts: function (data) {
-        switch (data.type) {
+    getId: function() {
+        const currentId = this.postId;
+        this.postId++;
+        return currentId;
+    },
+
+    addNewPosts: function(data) {
+        const newPostData = data;
+        newPostData.data.id = this.getId();
+        switch (newPostData.type) {
             case 'video':
-                this.postVideo.push(data.content);
+                this.postsVideo.push(newPostData.data.content.link);
                 break;
             case 'image':
-                this.postImage.push(data.content);
+                this.postsImage.push(newPostData.data.content.link);
+                break;
+            case 'audio':
+                this.postsAudio.push(newPostData.data.content.link);
                 break;
         }
 
-        this.postsList.push(data);
+        this.postsList.push(newPostData);
+        this.handlers.forEach(h => h(newPostData));
+      },
+
+      
+    handlers: [],
+    
+    listen: function(handler) {
+    this.handlers.push(handler);
     },
 
-    getPosts: function (id) {
-        const index = this.postsList.findIndex(item => item.data.id === id);
-        const nextIndex = index + 1;
-        return this.postsList.slice( nextIndex, nextIndex + 11);
+    getPosts: function (id = this.postsList.length) {
+        const nextIndex = id - 10;
+        return this.postsList.slice( nextIndex, id);
+    },
+
+    initSendPosts: function () {
+        this.handlers.forEach(h => h({status: 'init', data: this.getPosts()}));
     }
 }
+
+
+// module.exports = {
+//     eventSubscription: {
+//       data: [],
+//       add: function(item) {
+//         this.data.push(item);
+//         this.handlers.forEach(h => h(item));
+//       },
+
+//       handlers: [],
+
+//       listen: function(handler) {
+//         this.handlers.push(handler);
+//       },
+//       getAllData: function() {
+//         this.handlers.forEach(h => h({status: 'init', data: this.data}));
+//       }
+//     },
+//   }
 
 module.exports = {
     db,
--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 const Koa = require('koa');
 const koaBody = require('koa-body');
 const  Router = require('koa-router');
 const json = require('koa-json');
-const WS = require('ws');
+const koaStatic = require('koa-static');
+const { streamEvents } = require('http-event-stream');
 const router = new Router();
 const app = new Koa();
-const uuid = require('uuid');
+
 const { db } = require('./DB/db');
+const faker = require('faker');
+
+const public = path.join(__dirname, '/public');
+
+for (let i = 0 ; i < 15 ; i++) {
+  const obj = {
+    type: 'text',
+    data: {
+        content: faker.lorem.sentence(),
+        date: faker.date.past(),
+    }
+  }
+  db.addNewPosts(obj);
+}
 
 app.use( koaBody({
   urlencoded: true,
   multipart: true,
+  json: true,
 }));
+
+app.use( koaStatic(public));
 
 app.use(json());
 
@@ -25,7 +45,7 @@ app.use( async (ctx, next) => {
   const headers = { 'Access-Control-Allow-Origin': '*' };
 
   if (ctx.request.method !== 'OPTIONS') {
-    console.log('! OPTIONS');
+    // console.log('! OPTIONS');
     ctx.response.set({ ...headers });
     try {
       return await next();
@@ -51,9 +71,104 @@ app.use( async (ctx, next) => {
 
 const port = process.env.PORT || 7070;
 const server = http.createServer(app.callback());
-const wsServer = new WS.Server({
-  server
+
+
+app.use(router.routes()).use(router.allowedMethods());
+
+router.get('/sse', async (ctx) => {
+  streamEvents(ctx.req, ctx.res, {
+    async fetch(lastEventId) {
+      console.log(lastEventId);
+      return [];
+    },
+    stream(sse) {
+      db.listen( item => {
+        sse.sendEvent({
+          data: JSON.stringify(item),
+        });
+      });
+      return () => {};
+    }
+  });
+  db.initSendPosts();
+  ctx.respond = false;
 });
+
+// router.get('/public/:name', (ctx) => {
+//   const file = ctx.request.files.file;
+//   const body = JSON.parse(ctx.request.body.textData);
+//   const linkSourse = await downloadMedia(file);
+//   body.data.content.link = linkSourse
+
+//   db.addNewPosts(body)
+//   ctx.response.body = {status: 'ok'};
+// });
+
+router.post('/text', (ctx) => {
+    const item = JSON.parse(ctx.request.body);
+    console.log(item)
+    db.addNewPosts(item);
+    ctx.response.body = {status: 'ok'};
+});
+
+router.post('/media', async (ctx) => {
+  const file = ctx.request.files.file;
+  const body = JSON.parse(ctx.request.body.textData);
+  const linkSourse = await downloadMedia(file);
+  body.data.content.link = linkSourse
+
+  db.addNewPosts(body)
+  ctx.response.body = {status: 'ok'};
+});
+
+async function downloadMedia(file) {
+  const link = await new Promise((resolve, reject) => {
+    const oldPath = file.path;
+    const fileName = file.name;
+    const newPath = path.join(public, fileName);
+
+    const callback = (error) => reject(error);
+
+    const readStream = fs.createReadStream(oldPath);
+    const writeStream = fs.createWriteStream(newPath);
+
+    readStream.on('error', callback);
+    writeStream.on('error', callback);
+
+    writeStream.on('close', () => {
+      fs.unlink(oldPath, callback);
+      console.log('close');
+      resolve(fileName);
+    });
+
+    readStream.pipe(writeStream);
+  });
+  return link;
+}
+
+server.listen(port);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const wsServer = new WS.Server({
+//   server
+// });
+
+// wsServer.binaryType = 'arraybuffer';
+
 
 // router.post('/', async (ctx) => {
 //   const { nickname } = ctx.request.body;
@@ -63,8 +178,8 @@ const wsServer = new WS.Server({
 //   }
 //   // db.add(nickname);
 //   // chat.length = 0;
-//   console.log(chat)
-//   // console.log(nickname)
+  // console.log(chat)
+  // console.log(nickname)
 //   ctx.response.body = {
 //      status: true,
 //      users: db.data,
@@ -81,28 +196,69 @@ const wsServer = new WS.Server({
 
 // })
 
-app.use(router.routes()).use(router.allowedMethods());
+// wsServer.on('connection', (ws) => {
+//   ws.on('message', (e) => {
+//     // const data = JSON.parse(e)
+//     console.log('данные пришли')
+//     downloadFile(e);
+//     // if (data.type === 'image' || data.type === 'video' || data.type === 'audio') {
+//     //   downloadFile(data.data.content.sourse);
+//     // }
+//     // const sendData = db.addNewPosts(data);
 
-wsServer.on('connection', (ws) => {
-  ws.on('message', (e) => {
-    const data = JSON.parse(e)
-    console.log(data);
+//     // Array.from(wsServer.clients)
+//     // .filter(client => client.readyState === WS.OPEN)
+//     // .forEach(client => client.send(JSON.stringify({
+//     //   status: 'message',
+//     //    data: sendData,
+//     //  })));
+//   });
 
-    data.data.id = uuid.v4();
-    db.addNewPosts(data);
+//   ws.send(JSON.stringify({
+//     status: 'init',
+//      data: db.getPosts(),
+//    }));
+// });
 
-    Array.from(wsServer.clients)
-    .filter(client => client.readyState === WS.OPEN)
-    .forEach(client => client.send(JSON.stringify({
-      status: 'message',
-       data: data,
-     })));
-  });
 
-  ws.send(JSON.stringify({
-    status: 'init',
-     data: db.postsList,
-   }));
-});
 
-server.listen(port);
+// async function downloadFile(data) {
+//   console.log(data);
+//   // const byteArray = new Buffer(data.sourse.replace(/^[\w\d;:\/]+base64\,/g, ''), 'base64');
+//   // const sendFile = new File([byteArray], data.fileName, {type: data.filetype});
+  
+//   // async () => {
+//   //   const getResponse = await fetch(data.sourse);
+//   //   const blob = await getResponse.blob();
+//   //   const file = new File([blob], data.fileName, {type: data.filetype});
+//   //   return file;
+//   // }
+
+//   // const sendFile = Buffer.from(data.sourse)
+//   // console.log(sendFile);
+//   let i = new Uint8Array(data);
+
+//   i = data;
+
+//   const link = await new Promise((resolve, reject) => {
+//     const oldPath =  i;
+//     const fileName = "newf.jpg";
+//     const newPath = path.join(public, fileName);
+
+//     const callback = (error) => reject(error);
+
+//     const readStream = fs.createReadStream(oldPath);
+//     const writeStream = fs.createWriteStream(newPath);
+
+//     readStream.on('error', callback);
+//     writeStream.on('error', callback);
+
+//     writeStream.on('close', () => {
+//       fs.unlink(oldPath, callback);
+//       // console.log('close');
+//       resolve(fileName);
+//     });
+
+//     readStream.pipe(writeStream);
+//   });
+// }
